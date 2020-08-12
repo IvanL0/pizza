7310c45940ba3ac74f58d9bfc106c36b4d9d5138
IvanLo <ionkettler@gmail.com>
2020-08-12 14:07:23 +0300

    changes

---
+++ build/precache-manifest.1a3e2233285120883d1819378df27f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123b5503607e956a584300b1822c54c4",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "a5d2151795d22ae164ee",
+    "url": "/pizza/static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "634432abf2074d54c562",
+    "url": "/pizza/static/css/main.d8489af5.chunk.css"
+  },
+  {
+    "revision": "a5d2151795d22ae164ee",
+    "url": "/pizza/static/js/2.f680d284.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/pizza/static/js/2.f680d284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634432abf2074d54c562",
+    "url": "/pizza/static/js/main.65400e4d.chunk.js"
+  },
+  {
+    "revision": "51e8adb92ec1dbf344ed",
+    "url": "/pizza/static/js/runtime-main.207d1252.js"
+  }
+]);
